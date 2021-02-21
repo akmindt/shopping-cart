0dbce0d65e98fef2c4b242ac85c6cc279a354bed
akmindt <akmindt@gmail.com>
2021-02-21 10:55:40 -0700

    add navbar

---
+++ shopping-cart/src/app/sc-toolbar/sc-toolbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScToolbarComponent } from './sc-toolbar.component';
+
+describe('ScToolbarComponent', () => {
+  let component: ScToolbarComponent;
+  let fixture: ComponentFixture<ScToolbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ScToolbarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ScToolbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ shopping-cart/src/app/sc-toolbar/sc-toolbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sc-toolbar',
+  templateUrl: './sc-toolbar.component.html',
+  styleUrls: ['./sc-toolbar.component.scss']
+})
+export class ScToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  
+
+}
